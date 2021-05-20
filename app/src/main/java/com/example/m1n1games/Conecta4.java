bae6ae24811e64a3df4ead94934c3798bdaee97a
dam2a <thestevecraft5@gmail.com>
2021-05-20 17:42:27 +0200

    Subida

--- app/src/main/java/com/example/m1n1games/Conecta4.java
+++ app/src/main/java/com/example/m1n1games/Conecta4.java
@@ -1,7 +1,66 @@
 package com.example.m1n1games;
 
-import java.util.Scanner;
+import android.os.Bundle;
 
-public class Conecta4 {
+import androidx.fragment.app.Fragment;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.m1n1games.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link com.example.m1n1games.CrearCuenta#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Conecta4 extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Conecta4() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment CrearCuenta.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static com.example.m1n1games.CrearCuenta newInstance(String param1, String param2) {
+        com.example.m1n1games.CrearCuenta fragment = new com.example.m1n1games.CrearCuenta();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_conecta4, container, false);
+    }
 }
